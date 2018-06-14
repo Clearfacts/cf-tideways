1707ef26443173de38c3184c03fbf36450cf753e
Gert Vrebos <gert.vrebos@tactics.be>
2018-06-14 15:20:48 +0200

    Add env output

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 # default environment is production, override with environment variable
 set ENV=${TIDEWAYS_ENV:-production}
 
+echo "Starting Tideways Daemon with env=$ENV"
+
 tideways-daemon --hostname=tideways-daemon --address=0.0.0.0:9135 --env=$ENV >> /dev/stdout 2>/dev/stderr
 
 /sbin/my_init
